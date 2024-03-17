41505fadfee80200d08eaec3e7bca8b8e66e1574
R.Noto <rikitonoto@gmail.com>
2024-03-17 22:02:00 +0900

    stepをつけたファイル名で保存するオプションの追加

--- src/baselines/callbacks.py
+++ src/baselines/callbacks.py
@@ -13,6 +13,7 @@ class ModelSaveCallback(BaseCallback):
         verbose: int = 0,
         save_epoch: int = 10,
         save_only_best=True,
+        overwrite=True,
     ):
         super().__init__(verbose)
 
@@ -21,6 +22,7 @@ class ModelSaveCallback(BaseCallback):
         self.__save_dir_path = save_dir_path  # 保存先のディレクトリ
         self.__save_epoch = save_epoch  # 保存間隔
         self.__save_only_best = save_only_best  # 最高値の場合のみ保存
+        self.__overwrite = overwrite
 
         # Those variables will be accessible in the callback
         # (they are defined in the base class)
@@ -69,43 +71,11 @@ class ModelSaveCallback(BaseCallback):
             self.locals["self"].save(
                 os.path.join(self.__save_dir_path, "breakout_model")
             )
+            if not self.__overwrite:
+                self.locals["self"].save(
+                    os.path.join(
+                        self.__save_dir_path, f"breakout_model_{self.num_timesteps}"
+                    )
+                )
 
         return True
-
-
-# class ModelSaver:
-
-#     def __init__(
-#         self,
-#         log_dir_path: str,
-#         save_dir_path: str,
-#         save_epoch: int = 10,
-#         save_only_best=True,
-#     ) -> None:
-#         self.__update_count = 0  # 更新回数
-#         self.__best_mean_reward = -np.inf  # 最高の報酬
-#         self.__log_dir_path = log_dir_path  # ログファイルのディレクトリ
-#         self.__save_dir_path = save_dir_path  # 保存先のディレクトリ
-#         self.__save_epoch = save_epoch  # 保存間隔
-#         self.__save_only_best = save_only_best  # 最高値の場合のみ保存
-
-#     def callback(self, _locals, _globals):
-#         self.__update_count += 1
-
-#         if self.__update_count % self.__save_epoch > 0:
-#             return
-
-#         os.makedirs(self.__save_dir_path, exist_ok=True)
-
-#         _, y = ts2xy(load_results(self.__log_dir_path), "timesteps")
-
-#         if len(y) <= 0:
-#             return
-
-#         # 平均報酬がベスト平均報酬以上の時はモデルを保存
-#         mean_reward = np.mean(y[-self.__save_epoch :])
-
-#         if not self.__save_only_best or (mean_reward > self.__best_mean_reward):
-#             if mean_reward > self.__best_mean_reward:
-#                 self.__best_mean_reward = mean_reward
-#             _locals["self"].save(os.path.join(self.__save_dir_path, "breakout_model"))
